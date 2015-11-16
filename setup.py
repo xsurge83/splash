2abd4d82ad869d3d041903b7d353ecf98452a2e6
xsurge83 <xsurge83@gmail.com>
2015-11-15 21:08:49 -0800

    requirements clean up and use find packages in setup

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='splash',
     version='0.0.4',
-    packages=['splash', 'splash.commands'],
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
             'splash = splash.cli:cli'
